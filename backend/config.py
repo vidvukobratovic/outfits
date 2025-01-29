786cf7f6e7b9a86b2eb0e49acc2e4c969a397c8e
Vid Vukobratovic <vukobratovicvid@gmail.com>
2025-01-28 22:11:18 -0500

    configured cloudinary, updated .env

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+import cloudinary
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
